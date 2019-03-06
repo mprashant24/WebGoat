6bb3d7576424923853ba5302771374d0c8de7391
mprashant24 <mprashant24@users.noreply.github.com>
2019-03-06 00:15:02 -0500

    Update SqlInjectionLesson5a.java

--- webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/plugin/introduction/SqlInjectionLesson5a.java
+++ webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/plugin/introduction/SqlInjectionLesson5a.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.io.IOException;
 import java.sql.*;
 
+
 /***************************************************************************************************
  *
  *
